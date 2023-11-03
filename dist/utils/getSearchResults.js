f7cae05897a8c5030a86c97d359c4ffaf606553e
VriaA <oyelolaabimbola23@gmail.com>
2023-11-03 08:17:06 +0100

    Update getSearchResults.js

--- dist/utils/getSearchResults.js
+++ dist/utils/getSearchResults.js
@@ -11,16 +11,18 @@ export const options = {
   };
 
 export default async function getSearchResults() {
-    const searchInputEl = document.getElementById('search-input')
-
-    const title = searchInputEl.value
-    let filmTitle = title.trim().replace(/&/g, '%26')
-
-    const response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${filmTitle}&include_adult=false&language=en-US&page=1`, options)
-    const data = await response.json()
-    const searchResults = data.results.filter(result=> result.media_type != 'person')
-    
-    if(searchResults) {
-        sessionStorage.setItem('results', JSON.stringify(searchResults))
+    try {
+      const searchInputEl = document.getElementById('search-input')
+      const title = searchInputEl.value
+      const filmTitle = title.trim().replace(/&/g, '%26')
+  
+      const response = await fetch(`https://api.themoviedb.org/3/search/multi?query=${filmTitle}&include_adult=false&language=en-US&page=1`, options)
+      const data = await response.json()
+      const searchResults = data.results.filter(result=> result.media_type != 'person')
+      if(searchResults) {
+          sessionStorage.setItem('results', JSON.stringify(searchResults))
+      }
+    } catch (error) {
+      window.alert('Check internet connection and try again.')
     }
 }
