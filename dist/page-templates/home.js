332ff4f81e66725712e83a08b9cf68b219d4febb
VriaA <oyelolaabimbola23@gmail.com>
2023-11-03 06:47:44 +0100

    Update home.js

--- dist/page-templates/home.js
+++ dist/page-templates/home.js
@@ -1,11 +1,6 @@
 
 export default function getHomeHTML() {
     return `<div class="page-wrapper bg-homeImg bg-wrapperImgPosition md:bg-wrapperImgPositionMd">
-                <!-- <div id="loader" class="fixed top-0 left-0 w-screen h-screen bg-zinc-950 z-50
-                flex items-center justify-center">
-                <div class="absolute w-10 h-10 md:w-20 md:h-20 bg-red-800 rounded-full animate-leftPreload md:animate-leftPreloadMd"></div>
-                <div class="absolute w-10 h-10 md:w-20 md:h-20 bg-slate-100/10 rounded-full backdrop-blur-sm animate-rightPreload md:animate-rightPreloadMd"></div>
-                </div> -->
 
                 <div class="content-cntr flex justify-center">
                     <div class="w-full h-full flex flex-col justify-center items-center gap-4"> 
