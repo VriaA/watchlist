7f61c12cd0b5d7331fec490f04f0892e7b0bb893
VriaA <oyelolaabimbola23@gmail.com>
2023-11-05 01:20:14 +0100

    change results page header's animation ease

--- dist/animations/results.js
+++ dist/animations/results.js
@@ -8,7 +8,7 @@ function animateIn() {
     tl.fromTo(posters, {opacity: 0, y: 56}, {opacity: 1, y: 0, duration: .3, stagger: .2, ease: "power2.out"})
     tl.fromTo('.left-arrow', {opacity: 0}, {opacity: .3, duration: .2, ease: "power2.out"}, .6)
     tl.fromTo('.right-arrow', {opacity: 0}, {opacity: 1, duration: .2, ease: "power2.out"}, '<')
-    tl.fromTo('#results-header', {y: -56, opacity: 0,}, {y: 0, opacity: 1, duration: 1, ease: "bounce.in"}, 1)
+    tl.fromTo('#results-header', {y: -56, opacity: 0,}, {y: 0, opacity: 1, duration: 1, ease: "sine.in"}, 1)
 
     galleryCardsHoverAnimation(posters)
 }
