4f5ada3a457e319d5fbd23766e8bc4f84956fc18
VriaA <oyelolaabimbola23@gmail.com>
2023-11-12 14:00:38 +0100

    Update watchlist.js

--- dist/animations/watchlist.js
+++ dist/animations/watchlist.js
@@ -4,7 +4,7 @@ export default function animateIn() {
 
     const tl = gsap.timeline()
     
-    tl.from('#heading', {scale: .3, opacity: 0, duration: .3})
+    tl.from('#heading', {scale: .3, opacity: 0, duration: .3, delay: 2})
     tl.from(filtersCntr, {opacity: 0, duration: .2, stagger: .1, ease: "sine.out"})
     tl.fromTo(savedFilms, {opacity: 0, y: 56}, {opacity: 1, y: 0, duration: .5, stagger: .25, ease: "sine.out"})
 }
