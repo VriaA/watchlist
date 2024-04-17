77f9a45db3ec14c57f88ba429a4f364b7d1716d4
VriaA <oyelolaabimbola23@gmail.com>
2024-04-17 17:23:26 +0100

    change homepage structure and enter title message

--- watchlist/src/pages/Home.tsx
+++ watchlist/src/pages/Home.tsx
@@ -51,24 +51,26 @@ export default function Home():JSX.Element {
             </Link>
             <Link className="nav-link" to="watchlist">My watchlist</Link>
 
-            <form 
-                className="relative flex justify-end w-[68%] lg:w-[35%] h-10 bg-zinc-900/40 rounded-full overflow-hidden border border-stone-900/30"
-                onSubmit={handleFormSubmit} >
-                <span className="material-symbols-outlined absolute inset-0 my-auto w-fit h-fit left-2 z-50 self-center text-2xl md:text-3xl font-extralight">
-                    search
-                </span>
-                <input 
-                    className="w-full h-full px-10 text-[10px] min-[375px]:text-sm md:text-base bg-transparent outline-none text-center border-none" 
-                    type="text" 
-                    aria-label="Search Bar" 
-                    name="Movie Title" 
-                    placeholder="Movie or TV show title" 
-                    autoComplete="off"
-                    onChange={updateSearchTitleOnChange}
-                    value={searchTitle} 
-                />
-            </form>
-            <span ref={enterTitleMessageRef} className={`${isSearchBarEmpty ? 'opacity-1' : 'opacity-0'} mt-2 px-2 md:px-4 py-1 text-[10px] min-[375px]:text-sm md:text-base bg-zinc-900 font-normal leading-wide text-slate-50 rounded-full transition-opacity`}>Please enter a movie title to search.</span>
+            <main className='relative flex justify-center w-full'>
+                <form 
+                    className="relative flex justify-end w-[68%] lg:w-[35%] h-10 bg-zinc-900/40 rounded-full overflow-hidden border border-stone-900/30"
+                    onSubmit={handleFormSubmit} >
+                    <span className="material-symbols-outlined absolute inset-0 my-auto w-fit h-fit left-2 z-50 self-center text-2xl md:text-3xl font-extralight">
+                        search
+                    </span>
+                    <input 
+                        className="w-full h-full px-10 text-[10px] min-[375px]:text-sm md:text-base bg-transparent outline-none text-center border-none" 
+                        type="text" 
+                        aria-label="Search Bar" 
+                        name="Movie Title" 
+                        placeholder="Movie or TV show title" 
+                        autoComplete="off"
+                        onChange={updateSearchTitleOnChange}
+                        value={searchTitle} 
+                    />
+                </form>
+                <span ref={enterTitleMessageRef} className={`${isSearchBarEmpty ? 'opacity-1' : 'opacity-0'} absolute -bottom-8 md:-bottom-10 mt-2 px-2 md:px-4 py-1 text-[10px] min-[375px]:text-sm md:text-base bg-zinc-900 font-normal leading-wide text-slate-50 rounded-full transition-opacity`}>Please enter a title to search.</span>
+            </main>
         </div> 
     )
 }
