8c5093a43a239318024e519d6b8b202011aefdd6
VriaA <oyelolaabimbola23@gmail.com>
2024-04-18 08:27:57 +0100

    fix poster title bug

--- watchlist/src/components/results/Poster.tsx
+++ watchlist/src/components/results/Poster.tsx
@@ -9,7 +9,7 @@ export default function Poster({ result }: TPosterProps) {
             <span className="inline-block col-start-1 row-start-1 z-[2] min-h-[245px] lg:w-[242px] lg:h-[350px] flex-none bg-zinc-800/60 animate-pulse"></span>
             <img className="inline-block col-start-1 row-start-1 z-[3] w-full h-full object-cover object-center" src={`${imageBaseUrl}${poster_path}`} alt={`${(name || original_name) || (title || original_title)}`} />
             <span className="card film-name-poster min-h-[245px] lg:w-[242px] lg:h-[350px]" data-id={id} data-type={media_type || type}>
-                ${(name || original_name) || (title || original_title)}
+                {(name || original_name) || (title || original_title)}
             </span>
         </span>
     )
