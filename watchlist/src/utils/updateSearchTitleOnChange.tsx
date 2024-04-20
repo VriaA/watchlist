fe6c44e36b67a6b58f08ceaa3d9f0c6e69d466f8
VriaA <oyelolaabimbola23@gmail.com>
2024-04-20 15:19:23 +0100

    update search title

--- watchlist/src/utils/updateSearchTitleOnChange.tsx
+++ watchlist/src/utils/updateSearchTitleOnChange.tsx
@@ -3,5 +3,5 @@ import { ChangeEvent } from 'react'
 // CONTROLS THE SEARCH INPUT
 export default function updateSearchTitleOnChange(e: ChangeEvent, setTitle: React.Dispatch<React.SetStateAction<string>>) {
     const searchBar = e.target as HTMLInputElement
-    setTitle(searchBar.value)
+    setTitle(searchBar.value.toLowerCase().trim().replace('&', '%26'))
 }
--- watchlist/src/utils/useSearch.tsx
+++ watchlist/src/utils/useSearch.tsx
@@ -11,59 +11,56 @@ export default function useSearch(enterTitleMessageRef: RefObject<HTMLSpanElemen
     const isEmptySearchBar = searchTitle.trim().split('').length <= 0
     const location = useLocation()
 
-        // HIDES THE ENTER TITLE MESSAGE AFTER FOUR SECONDS
-        useEffect(()=> {
-            if(!isSearchBarEmpty) return
-                const clearMessage: NodeJS.Timeout = setTimeout(()=> hideEnterTitleMessage() , 4000)
-                return ()=> clearTimeout(clearMessage)
-        }, [isSearchBarEmpty])
+    // HIDES THE ENTER TITLE MESSAGE AFTER FOUR SECONDS
+    useEffect(()=> {
+        if(!isSearchBarEmpty) return
+            const clearMessage: NodeJS.Timeout = setTimeout(()=> hideEnterTitleMessage() , 4000)
+            return ()=> clearTimeout(clearMessage)
+    }, [isSearchBarEmpty])
         
-        // HIDES THE ENTER TITLE MESSAGE ONCE THERE IS TEXT IN THE SEARCH BAR
-        useEffect(()=> {
-            if (isEmptySearchBar) return 
-                hideEnterTitleMessage()
+    // HIDES THE ENTER TITLE MESSAGE ONCE THERE IS TEXT IN THE SEARCH BAR
+    useEffect(()=> {
+        if (isEmptySearchBar) return 
+            hideEnterTitleMessage()
 
-            async function getSearchSuggestions() {
-                const title: string = searchTitle.toLowerCase().trim().replace('&', '&26%')
-                const suggestions = await getSearchResults(title)
-                
-                if(Array.isArray(suggestions)) {
-                    setSuggestions(suggestions.splice(0, 3))
-                }
+        async function getSearchSuggestions() {
+            const suggestions = await getSearchResults(searchTitle)
+
+            if(Array.isArray(suggestions)) {
+                setSuggestions(suggestions.splice(0, 3))
             }
+        }
             
-            if(location.pathname !== '/') {
-               const suggestionsTimeOut = setTimeout(()=> getSearchSuggestions(), 300) 
-               return ()=> clearTimeout(suggestionsTimeOut)
-            }  
-        }, [searchTitle])
+        if(location.pathname !== '/') {
+           const suggestionsTimeOut = setTimeout(()=> getSearchSuggestions(), 300) 
+           return ()=> clearTimeout(suggestionsTimeOut)
+        }  
+    }, [searchTitle])
         
-        // HIDES THE ENTER TITLE MESSAGE IF IT IS VISIBLE
-        function hideEnterTitleMessage() {
-            if(enterTitleMessageRef.current && enterTitleMessageRef.current.classList.contains('opacity-1')) {
-                enterTitleMessageRef.current.classList.remove('opacity-1')
-                enterTitleMessageRef.current.classList.add('opacity-0')
-                setIsSearchBarEmpty(false)
-            }
+    // HIDES THE ENTER TITLE MESSAGE IF IT IS VISIBLE
+    function hideEnterTitleMessage() {
+        if(enterTitleMessageRef.current && enterTitleMessageRef.current.classList.contains('opacity-1')) {
+            enterTitleMessageRef.current.classList.remove('opacity-1')
+            enterTitleMessageRef.current.classList.add('opacity-0')
+            setIsSearchBarEmpty(false)
         }
+    }
     
-        // IF THE SEARCH BAR IS NOT EMPTY, THE USER IS REDIRECTED TO THE RESULTS PAGE OR THE URL SEARCH PARAMETER IS UPDATED
-        // IF THE SEARCH BAR IS EMPTY, isSearchBarEmpty IS SET TO TRUE WHICH RESULTS IN THE DISPLAY OF THE ENTER TITLE MESSAGE
-        // THIS PREVENTS SEARCHING WHEN THE SEARCH BAR IS EMPTY OR FILLED WITH ONLY WHITESPACES
-        // IF THE SEARCH BAR IS NOT EMPTY, THE USER IS REDIRECTED TO THE RESULTS PAGE OR THE URL SEARCH PARAMETER IS UPDATED 
-        const handleSearchFormSubmit: THandleSearchFormSubmit = (e, navigate, setSearchParams)=> {
-            e.preventDefault()
+    // IF THE SEARCH BAR IS NOT EMPTY, THE USER IS REDIRECTED TO THE RESULTS PAGE OR THE URL SEARCH PARAMETER IS UPDATED
+    // IF THE SEARCH BAR IS EMPTY, isSearchBarEmpty IS SET TO TRUE WHICH RESULTS IN THE DISPLAY OF THE ENTER TITLE MESSAGE
+    // THIS PREVENTS SEARCHING WHEN THE SEARCH BAR IS EMPTY OR FILLED WITH ONLY WHITESPACES
+    // IF THE SEARCH BAR IS NOT EMPTY, THE USER IS REDIRECTED TO THE RESULTS PAGE OR THE URL SEARCH PARAMETER IS UPDATED 
+    const handleSearchFormSubmit: THandleSearchFormSubmit = (e, navigate, setSearchParams)=> {
+        e.preventDefault()
     
-            const title: string = searchTitle.toLowerCase().trim().replace('&', '&26%')
-    
-            if(!isEmptySearchBar) {
-                setIsSearchBarEmpty(false);
-                (document.getElementById('search-input') as HTMLInputElement).value = ''
-                navigate ? navigate(`search?title=${title}`) : setSearchParams ? setSearchParams({title: title}) : ''
-            } else {
-                setIsSearchBarEmpty(true)
-            }
+        if(!isEmptySearchBar) {
+            setIsSearchBarEmpty(false);
+            (document.getElementById('search-input') as HTMLInputElement).value = ''
+            navigate ? navigate(`search?title=${searchTitle}`) : setSearchParams ? setSearchParams({title: searchTitle}) : ''
+        } else {
+            setIsSearchBarEmpty(true)
         }
+    }
 
-        return { searchTitle, setSearchTitle, isSearchBarEmpty, setIsSearchBarEmpty, handleSearchFormSubmit, suggestions } as TUseSearch
+    return { searchTitle, setSearchTitle, isSearchBarEmpty, setIsSearchBarEmpty, handleSearchFormSubmit, suggestions } as TUseSearch
 }
