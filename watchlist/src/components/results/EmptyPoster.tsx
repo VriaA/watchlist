f0b8a60bc98a64e9f5b9a54b0483193fdae89fb4
VriaA <oyelolaabimbola23@gmail.com>
2024-04-24 11:00:12 +0100

    add a Link to the film page to posters

--- watchlist/src/components/results/EmptyPoster.tsx
+++ watchlist/src/components/results/EmptyPoster.tsx
@@ -3,13 +3,16 @@ import { TPosterProps } from '../../types/resultTypes'
 
 export default function EmptyPoster({ result }: TPosterProps): JSX.Element {
     const { id, media_type, type, name, original_name, title, original_title } = result
+
+    const filmName: string = name || original_name || title || original_title
+    const filmType: string = media_type||type
     return (
-        <Link to={`/${media_type||type}/${id}`} className="card relative min-h-[245px] lg:w-[242px] lg:h-[350px] no-poster" data-id={id} data-type={media_type || type}>
-            <span className="absolute inset-0 m-auto h-fit w-fit material-symbols-outlined text-5xl lg:text-7xl text-white font-thin" data-id={id} data-type={media_type || type}>
+        <Link to={`/${filmType}/${filmName.toLowerCase()}+${id}`} className="card relative min-h-[245px] lg:w-[242px] lg:h-[350px] no-poster">
+            <span className="absolute inset-0 m-auto h-fit w-fit material-symbols-outlined text-5xl lg:text-7xl text-white font-thin">
                 broken_image
             </span>
-            <p className="text-xs lg:text-sm capitalise font-normal p-2" data-result-id={id} data-result-type={media_type || type}>
-                {(name || original_name) || (title || original_title)}
+            <p className="text-xs lg:text-sm capitalise font-normal p-2">
+                {filmName}
             </p>
         </Link>
     )
--- watchlist/src/components/results/Poster.tsx
+++ watchlist/src/components/results/Poster.tsx
@@ -5,12 +5,14 @@ import imageBaseUrl from '../../utils/imageBaseUrl'
 export default function Poster({ result }: TPosterProps) {
     const { id, media_type, type, name, original_name, title, original_title, poster_path } = result
     
+    const filmName: string = name || original_name || title || original_title
+    const filmType: string = media_type || type
     return (
-        <Link to={`/${media_type||type}/${id}`} className="group grid z-[5] card min-h-[245px] lg:w-[242px] lg:h-[350px] flex-none overflow-hidden cursor-pointer rounded-md snap-start">
+        <Link to={`/${filmType}/${filmName.toLowerCase()}+${id}`} className="group grid z-[5] card min-h-[245px] lg:w-[242px] lg:h-[350px] flex-none overflow-hidden cursor-pointer rounded-md snap-start">
             <span className="inline-block col-start-1 row-start-1 z-[2] min-h-[245px] lg:w-[242px] lg:h-[350px] flex-none bg-zinc-800/60 animate-pulse"></span>
-            <img className="inline-block col-start-1 row-start-1 z-[3] w-full h-full object-cover object-center" src={`${imageBaseUrl}${poster_path}`} alt={`${(name || original_name) || (title || original_title)}`} />
+            <img className="inline-block col-start-1 row-start-1 z-[3] w-full h-full object-cover object-center" src={`${imageBaseUrl}${poster_path}`} alt={`${filmName}`} />
             <span className="card film-name-poster min-h-[245px] lg:w-[242px] lg:h-[350px]">
-                {(name || original_name) || (title || original_title)}
+                {filmName}
             </span>
         </Link>
     )
