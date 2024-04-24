42da06070ee6126774299b6384cfc80f26e15588
VriaA <oyelolaabimbola23@gmail.com>
2024-04-24 11:59:30 +0100

    create custom hook for fetching data

---
+++ watchlist/src/hooks/useFetch.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+// TODO: Use custom hook in home & results page.
+// const searchResults: U = data.results.filter((result: V)=> result.media_type != 'person')
+// `https://api.themoviedb.org/3/search/multi?query=${title}&include_adult=false&language=en-US&page=1`
+export default function useFetch(url: string) {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjhiOTQxM2EwYzk3NTE1YzUwNTkyZTRlY2Y5MjM0MSIsInN1YiI6IjY0ZDA1YTJhNGQ2NzkxMDBlMjQwZjE2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xbqgGX1THEkUWs4NigowqE8P8tLn8hYScBzSfL5dYmI'
+        }
+    };
+
+    const [results, setResults] = useState([])
+    const [error, setError] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(()=> {
+        async function getResults() {
+            setLoading(true)
+            try {
+                const response: Response = await fetch(url, options)
+                handleResponseError(response)
+                const data = await response.json()
+                setResults(data)
+            } catch (error: any) {
+                setError(error.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        getResults()
+    }, [url])
+
+    // THROWS AN ERROR BASED ON THE ERROR CODE RECEIVED DURING THE FETCHING PROCESS
+    function handleResponseError(response: Response) {
+        if(response.ok) return
+        if(response.status >= 500) {
+            throw new Error('Unable to connect to the server.')
+        } else if (response.status === 429) {
+            throw new Error('Too many requests: Please try again later.');
+        } else {
+            throw new Error('Error fetching results. \n Please check your internet connection and try again.')
+        }
+    }
+
+    return { results, error, loading }
+}
--- watchlist/src/pages/FIlm.tsx
+++ watchlist/src/pages/FIlm.tsx
@@ -1,5 +1,20 @@
+import { useLocation } from "react-router-dom"
+import useFetch from "../hooks/useFetch"
+
 export default function Film(): JSX.Element {
+    const location = useLocation()
+    const type: string = location.pathname.trim().split('/')[1]
+    const id: string = location.pathname.trim().split('/')[2].split('+')[1]
+    const url: string = `https://api.themoviedb.org/3/${type}/${id}?append_to_response=credits%2Cvideos%2Csimilar&language=en-US`
+    const { results, error, loading } = useFetch(url)
+
     return (
-        <h1>Film Page</h1>
+        <>
+            {
+                loading ? <h1>loading...</h1> :
+                results ? <h1>{`${results}`}</h1> :
+                <h1>{error}</h1>
+            }
+        </>
     )
 }
