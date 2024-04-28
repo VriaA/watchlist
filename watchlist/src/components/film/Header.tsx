ac375a848823bd90e9726aa6f6772bd109111103
VriaA <oyelolaabimbola23@gmail.com>
2024-04-28 11:34:39 +0100

    toggle search bar visibility on button click

--- watchlist/src/components/film/Header.tsx
+++ watchlist/src/components/film/Header.tsx
@@ -1,6 +1,17 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 export default function Header() {
+    const searchFieldRef = useRef<HTMLFieldSetElement | null>(null)
+
+    function toggleSearchBarVisibility() {
+        const isPC = window.innerWidth >= 1024
+        const searchField = searchFieldRef.current
+        if(!searchField) return
+            searchField.classList.toggle('hidden')
+            searchField.style.width = isPC ? '500px' : '250px'     
+    }
+
     return (
         <header className="absolute flex justify-end items-center gap-4 w-full p-[4%] md:p-[2%] lg:snap-start">
                         
@@ -8,15 +19,15 @@ export default function Header() {
 
                     <form className="relative h-10 min-w-[38px] cursor-pointer" title="Search">
                     
-                        <button type="button" className="absolute inset-0 my-auto left-2 z-50 w-fit h-fit transition-transform hover:-translate-y-1 active:translate-y-1">
+                        <button onClick={toggleSearchBarVisibility} type="button" className="absolute inset-0 my-auto left-2 z-50 w-fit h-fit transition-transform hover:-translate-y-1 active:translate-y-1">
                             <span className="material-symbols-outlined text-3xl font-light md:font-extralight">
                                 search 
                             </span> 
                         </button>
 
-                        <fieldset className="hidden relative w-1 h-full px-2 z-20 bg-zinc-900/30 backdrop-blur-md rounded-2xl overflow-hidden border border-stone-900/30">
-                                <input className="w-full h-full ml-2 px-10 font-light bg-transparent text-[10px] min-[375px]:text-sm md:text-base caret-slate-100 outline-none border-none" 
-                                                        type="text" aria-label="Search Bar" name="Movie Title" placeholder="Movie or TV show title" autoComplete="off" />
+                        <fieldset ref={searchFieldRef} className="hidden relative w-1 h-full px-2 z-20 bg-zinc-900/30 backdrop-blur-md rounded-2xl overflow-hidden border border-stone-900/30">
+                            <input className="w-full h-full ml-2 px-10 font-light bg-transparent text-[10px] min-[375px]:text-sm md:text-base caret-slate-100 outline-none border-none" 
+                                    type="text" aria-label="Search Bar" name="Movie Title" placeholder="Movie or TV show title" autoComplete="off" />
                         </fieldset> 
                         <ul id="suggestions" className="to-hide films search-suggestions invisible z-10">
                         </ul>   
