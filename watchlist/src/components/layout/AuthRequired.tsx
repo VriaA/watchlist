63d7420c53ee31615542750a6af42c9ce18b4bea
VriaA <123843734+VriaA@users.noreply.github.com>
2024-06-30 12:09:28 +0100

    adjust watchlist style

--- watchlist/src/components/layout/AuthRequired.tsx
+++ watchlist/src/components/layout/AuthRequired.tsx
@@ -8,8 +8,12 @@ export default function AuthRequired(): JSX.Element {
     const { isLoggedIn } = useContext(AppContext) as TAppContext
 
     return (
-        <>
-            {isLoggedIn === null ? <Loader /> : isLoggedIn ? <Outlet /> : <Navigate to="/sign-in" replace={true} />}
-        </>
+        <div className="w-screen h-screen page-wrapper bg-homeImg bg-wrapperImgPosition md:bg-wrapperImgPositionMd">
+            <div className="w-screen h-screen grid place-content-center">
+                <div className="content-cntr overflow-y-auto">
+                    {isLoggedIn === null ? <Loader /> : isLoggedIn ? <Outlet /> : <Navigate to="/sign-in" replace={true} />}
+                </div>
+            </div>
+        </div>
     )
 }
--- watchlist/src/pages/Watchlist.tsx
+++ watchlist/src/pages/Watchlist.tsx
@@ -3,12 +3,6 @@ import WatchlistFilms from "../components/watchlist/WatchlistFilms"
 export default function Watchlist(): JSX.Element {
 
     return (
-        <div className="w-screen h-screen page-wrapper bg-homeImg bg-wrapperImgPosition md:bg-wrapperImgPositionMd">
-            <div className="w-screen h-screen grid place-content-center">
-                <div className="content-cntr overflow-y-auto">
-                    <WatchlistFilms />
-                </div>
-            </div>
-        </div>
+        <WatchlistFilms />
     )
 }
