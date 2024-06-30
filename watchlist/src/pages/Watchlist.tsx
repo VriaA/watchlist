a993030f7d57586581bb4d8b1f18982c5f17b97f
VriaA <123843734+VriaA@users.noreply.github.com>
2024-06-30 12:51:10 +0100

    render watchlist films when userWatchlist is not null

--- watchlist/src/pages/Watchlist.tsx
+++ watchlist/src/pages/Watchlist.tsx
@@ -1,8 +1,14 @@
 import WatchlistFilms from "../components/watchlist/WatchlistFilms"
+import { AppContext } from "../contexts/AppContext"
+import { TAppContext } from "../types/appTypes"
+import { useContext } from "react"
 
 export default function Watchlist(): JSX.Element {
+    const { userWatchlist } = useContext(AppContext) as TAppContext
 
     return (
-        <WatchlistFilms />
+        <>
+            {userWatchlist && <WatchlistFilms />}
+        </>
     )
 }
