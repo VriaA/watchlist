bcf354ae52bb52fd235295c10fcc72e059935a3e
VriaA <123843734+VriaA@users.noreply.github.com>
2024-07-02 10:35:17 +0100

    add max-height to saved film poster

--- watchlist/src/components/watchlist/SavedFilmPoster.tsx
+++ watchlist/src/components/watchlist/SavedFilmPoster.tsx
@@ -9,12 +9,12 @@ export default function SavedFilmPoster({ film }: { film: DocumentData }): JSX.E
         <>
             {
                 poster ?
-                    <Link to={`/${filmType}/${name}+${filmId}`} className="grid grid-cols-1 grid-rows-1 min-h-[200px]">
-                        <img className="col-start-1 row-start-1 z-[8]  h-full object-cover object-center rounded-t-md cursor-pointer" src={poster} alt={name} title={name} loading="lazy"></img>
-                        <span className="block col-start-1 row-start-1 z-[7] h-full bg-zinc-900/40 animate-pulse"></span>
+                    <Link to={`/${filmType}/${name}+${filmId}`} className="grid grid-cols-1 grid-rows-1 min-h-[200px] max-h-[280px]">
+                        <img className="col-start-1 row-start-1 z-[8] w-full h-full object-cover object-center rounded-t-md cursor-pointer" src={poster} alt={name} title={name} loading="lazy"></img>
+                        <span className="block col-start-1 row-start-1 z-[7] w-full h-full bg-zinc-900/40 animate-pulse"></span>
                     </Link>
                     :
-                    <Link to={`/${filmType}/${name}+${filmId}`} className="flex items-center justify-center min-h-[200px] bg-zinc-800/40 cursor-pointer">
+                    <Link to={`/${filmType}/${name}+${filmId}`} className="flex items-center justify-center min-h-[200px] max-h-[280px] bg-zinc-800/40 cursor-pointer">
                         <span className="material-symbols-outlined text-5xl lg:text-7xl font-thin">
                             broken_image
                         </span>
