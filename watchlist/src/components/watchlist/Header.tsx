9b7613d206e6b1416dec35b4fb5388f07fabf1c7
VriaA <123843734+VriaA@users.noreply.github.com>
2024-07-03 13:52:22 +0100

    style watchlist filter buttons based on the selected filter

--- watchlist/src/components/watchlist/Header.tsx
+++ watchlist/src/components/watchlist/Header.tsx
@@ -1,20 +1,23 @@
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import { AppContext } from "../../contexts/AppContext"
 import { TAppContext } from "../../types/appTypes"
 import WatchlistUser from "../WatchlistUser"
 
 export default function Header(): JSX.Element {
     const { userWatchlist } = useContext(AppContext) as TAppContext
+    const [searchParams] = useSearchParams()
+    const filter = searchParams.get('filter')
+
     return (
         <header className="flex justify-between items-center gap-2 w-full font-robotoCondensed mt-4 mb-[3svh] lg:mb-[4svh]">
             <nav className="flex flex-col gap-4">
                 <h1 className="text-2xl md:text-3xl uppercase font-inter font-medium">My watchlist</h1>
 
                 <ul className={`${!userWatchlist ? 'hidden' : ''} flex gap-2 text-sm md:text-base flex-wrap`}>
-                    <li><Link to="." className="watchlist-filter peer-checked/all:bg-zinc-100 peer-checked/all:text-zinc-900">All</Link></li>
-                    <li><Link to="?filter=watched" className="watchlist-filter peer-checked/watched:bg-zinc-100 peer-checked/watched:text-zinc-900">Watched</Link></li>
-                    <li><Link to="?filter=notwatched" className="watchlist-filter peer-checked/not-watched:bg-zinc-100 peer-checked/not-watched:text-zinc-900">Not watched</Link></li>
+                    <li><Link to="." className={`watchlist-filter ${!filter ? 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200' : ''}`}>All</Link></li>
+                    <li><Link to="?filter=watched" className={`watchlist-filter ${filter === 'watched' ? 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200' : ''}`}>Watched</Link></li>
+                    <li><Link to="?filter=notwatched" className={`watchlist-filter ${filter === 'notwatched' ? 'bg-zinc-100 text-zinc-900 hover:bg-zinc-200' : ''}`}>Not watched</Link></li>
                 </ul>
             </nav>
 
