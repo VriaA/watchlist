3e0aae5df00cb75dc9592efd30e5a22f3be6991a
VriaA <123843734+VriaA@users.noreply.github.com>
2024-07-04 18:24:16 +0100

    replace images with svg code

--- watchlist/src/components/film/Cast.tsx
+++ watchlist/src/components/film/Cast.tsx
@@ -2,47 +2,47 @@ import { TCast, TCrew } from "../../types/filmTypes"
 import imageBaseUrl from "../../utils/imageBaseUrl"
 import useScroll from "../../hooks/useScroll"
 
-export default function Cast({credits}: {credits: { cast: TCast[], crew: TCrew[]}}) {
+export default function Cast({ credits }: { credits: { cast: TCast[], crew: TCrew[] } }) {
     const { galleryRef, leftArrowRef, rightArrowRef, changeArrowOpacity, scrollLeft, scrollRight } = useScroll()
     const hasCast = credits && credits.cast.length > 0
 
-    function CastWithImage({profile_path, character, name}: TCast): JSX.Element {
-        return  (<div className="card card-cast snap-start flex flex-col flex-none gap-1 w-[49%] lg:w-[32%] bg-zinc-900/40 backdrop-blur-md rounded-md pb-2 text-center">
-                    <img className="w-full h-48 md:h-72 object-cover object-center rounded-t-md" src={`${imageBaseUrl}${profile_path}`} alt={name + ' image'} loading="lazy" />
-                    <p className="font-medium">{name}</p>
-                    <p className="font-extralight line-clamp-3">{character}</p>
-                </div>)
+    function CastWithImage({ profile_path, character, name }: TCast): JSX.Element {
+        return (<div className="card card-cast snap-start flex flex-col flex-none gap-1 w-[49%] lg:w-[32%] bg-zinc-900/40 backdrop-blur-md rounded-md pb-2 text-center">
+            <img className="w-full h-48 md:h-72 object-cover object-center rounded-t-md" src={`${imageBaseUrl}${profile_path}`} alt={name + ' image'} loading="lazy" />
+            <p className="font-medium">{name}</p>
+            <p className="font-extralight line-clamp-3">{character}</p>
+        </div>)
     }
-    
-    function CastNoImage({character, name}: TCast): JSX.Element {
-        return  (<div className="card card-cast snap-start flex flex-col flex-none gap-1 w-[49%] lg:w-[32%] bg-zinc-900/40 backdrop-blur-md rounded-md pb-2 text-center">
-                    <div className="grid place-content-center w-full h-48 md:h-72 rounded-t-md">
-                        <span className="material-symbols-outlined text-5xl lg:text-7xl font-thin" aria-hidden={true}>
-                            broken_image
-                        </span>
-                    </div>
-                    <p className="font-medium">{name}</p>
-                    <p className="font-extralight line-clamp-3">{character}</p>
-                </div>)
+
+    function CastNoImage({ character, name }: TCast): JSX.Element {
+        return (<div className="card card-cast snap-start flex flex-col flex-none gap-1 w-[49%] lg:w-[32%] bg-zinc-900/40 backdrop-blur-md rounded-md pb-2 text-center">
+            <div className="grid place-content-center w-full h-48 md:h-72 rounded-t-md">
+                <span className="material-symbols-outlined text-5xl lg:text-7xl font-thin" aria-hidden={true}>
+                    broken_image
+                </span>
+            </div>
+            <p className="font-medium">{name}</p>
+            <p className="font-extralight line-clamp-3">{character}</p>
+        </div>)
     }
 
     function NoCastData(): JSX.Element {
-        return ( <p className="card-cast w-full h-full text-lg lg:text-xl font-light text-center">
-                    No cast data.
-                 </p>)
+        return (<p className="card-cast w-full h-full text-lg lg:text-xl font-light text-center">
+            No cast data.
+        </p>)
     }
-    
+
     function CastELements(): JSX.Element[] | JSX.Element {
-        if(credits.cast.length > 0 ) {
-            return credits.cast.map((cast, i): JSX.Element=> {
-                const {profile_path, character, name} = cast
+        if (credits.cast.length > 0) {
+            return credits.cast.map((cast, i): JSX.Element => {
+                const { profile_path, character, name } = cast
                 return profile_path ? <CastWithImage key={i} profile_path={profile_path} character={character} name={name} /> : <CastNoImage key={i} character={character} name={name} />
             })
         } else {
             return <NoCastData />
         }
     }
-    
+
     return (
         <section className="flex items-center lg:h-[90svh] px-[2%] lg:snap-start">
             <h2 className="relative w-[30%] text-3xl md:text-6xl font-robotoCondensed font-normal">
@@ -52,25 +52,25 @@ export default function Cast({credits}: {credits: { cast: TCast[], crew: TCrew[]
 
             <div className="w-[70%] gallery-cntr hide-film-gallery">
 
-                {hasCast && <div className="arrows-cast flex justify-end items-end gap-3">
-                        <button ref={leftArrowRef} onClick={scrollLeft} title="Previous" aria-label="Previous cast">
-                            <span className="left-arrow scroll-arrow-small material-symbols-outlined" aria-hidden={true}>
-                                keyboard_arrow_left
-                            </span> 
-                        </button>
-                        
-                        <button ref={rightArrowRef} onClick={scrollRight} title="Next" aria-label="Next cast">
-                            <span className="right-arrow scroll-arrow-small material-symbols-outlined" aria-hidden={true}>
-                                keyboard_arrow_right
-                            </span> 
-                        </button>
+                {hasCast && <div className="flex justify-end items-center gap-3">
+                    <button ref={leftArrowRef} onClick={scrollLeft} title="Previous" aria-label="Previous cast">
+                        <svg className="hidden lg:inline-block cursor-pointer" aria-hidden={true} xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="36px" fill="#f8fafc">
+                            <path d="M560.67-240 320-480.67l240.67-240.66L608-674 414.67-480.67 608-287.33 560.67-240Z" />
+                        </svg>
+                    </button>
+
+                    <button ref={rightArrowRef} onClick={scrollRight} title="Next" aria-label="Next cast">
+                        <svg className="hidden lg:inline-block cursor-pointer" aria-hidden={true} xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="36px" fill="#f8fafc">
+                            <path d="M521.33-480.67 328-674l47.33-47.33L616-480.67 375.33-240 328-287.33l193.33-193.34Z" />
+                        </svg>
+                    </button>
                 </div>}
 
                 <div className="gallery lg:snap-x lg:snap-mandatory overflow-x-auto" ref={galleryRef} onScroll={changeArrowOpacity}>
                     <div className="cards-cntr flex gap-2 md:gap-5 pl-4 py-2">
                         <CastELements />
-                    </div>  
-                </div>  
+                    </div>
+                </div>
             </div>
         </section>
     )
--- watchlist/src/pages/Authentication.tsx
+++ watchlist/src/pages/Authentication.tsx
@@ -3,8 +3,6 @@ import { Link, useLocation } from "react-router-dom"
 import { deleteDoc, doc } from "firebase/firestore"
 import { app, db } from "../firebase"
 import { User, getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, reauthenticateWithCredential, deleteUser, AuthCredential, EmailAuthProvider, reauthenticateWithPopup, UserCredential } from "firebase/auth"
-import Loader from "../assets/images/loader.svg"
-import GoogleLogo from "../assets/images/google.svg"
 import { useNavigate } from "react-router-dom"
 import { AppContext } from "../contexts/AppContext"
 import { TAppContext } from "../types/appTypes"
@@ -217,7 +215,12 @@ export default function Authentication(): JSX.Element {
                         </fieldset>
                         <button className={`${loading ? 'cursor-not-allowed' : ''} grid place-content-center box-border h-12 leading-none w-full mt-10 py-2 bg-red-800 hover:bg-red-900 text-slate-50 font-inter font-semibold rounded-lg transition-all active:translate-y-1`}
                             disabled={loading ? true : false}>
-                            {loading ? <img className="w-8 h-8" src={Loader} alt="Loading..." /> : isSignIn ? 'Sign in' : isDeleteAccount ? 'Confirm' : 'Create account'}
+                            {loading ? <svg aria-label="Loading..." xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="32" height="32" style={{ shapeRendering: 'auto', display: 'block', background: 'transparent' }} xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" stroke="#ffffff" fill="none" cy="50" cx="50">
+                                <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+                            </circle><g></g></g></svg>
+                                : isSignIn ? 'Sign in'
+                                    : isDeleteAccount ? 'Confirm'
+                                        : 'Create account'}
                         </button>
                     </form>
                     {!isDeleteAccount &&
@@ -231,7 +234,15 @@ export default function Authentication(): JSX.Element {
                     <button
                         className={`${loading ? 'cursor-not-allowed' : ''} flex gap-2 items-center text-sm font-inter font-light text-slate-50 hover:underline hover:underline-offset-2`}
                         onClick={authenticateWithGoogle}
-                        disabled={loading ? true : false}><img src={GoogleLogo} alt="Google logo" /> Continue with google</button>
+                        disabled={loading ? true : false}>
+                        <svg aria-label="Google logo" width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.9996 19.6363V28.9309H36.916C36.3488 31.9199 34.6468 34.4509 32.0941 36.1527L39.8831 42.1964C44.4213 38.0075 47.0395 31.8547 47.0395 24.5456C47.0395 22.8438 46.8868 21.2073 46.6031 19.6366L23.9996 19.6363Z" fill="#4285F4" />
+                            <path d="M10.5494 28.568L8.79263 29.9128L2.57434 34.7564C6.52342 42.589 14.6174 48 23.9991 48C30.4789 48 35.9116 45.8618 39.8826 42.1964L32.0936 36.1528C29.9554 37.5927 27.2281 38.4656 23.9991 38.4656C17.7591 38.4656 12.4575 34.2547 10.5592 28.5819L10.5494 28.568Z" fill="#34A853" />
+                            <path d="M2.57436 13.2436C0.938084 16.4726 0 20.1163 0 23.9999C0 27.8834 0.938084 31.5271 2.57436 34.7561C2.57436 34.7777 10.5599 28.5597 10.5599 28.5597C10.08 27.1197 9.79624 25.5925 9.79624 23.9996C9.79624 22.4067 10.08 20.8795 10.5599 19.4395L2.57436 13.2436Z" fill="#FBBC05" />
+                            <path d="M23.9996 9.55636C27.5342 9.55636 30.676 10.7781 33.1851 13.1345L40.0577 6.2619C35.8904 2.37833 30.4797 0 23.9996 0C14.6179 0 6.52342 5.38908 2.57434 13.2437L10.5597 19.44C12.4578 13.7672 17.7596 9.55636 23.9996 9.55636Z" fill="#EA4335" />
+                        </svg>
+                        Continue with google
+                    </button>
                 </section>
             </div>
         </div>
