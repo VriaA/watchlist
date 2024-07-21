4922e69f1304d1f8c5e366053d33d982fba9ea0a
VriaA <123843734+VriaA@users.noreply.github.com>
2024-07-21 16:49:56 +0100

    animate film page hero elements

--- watchlist/src/components/film/Hero.tsx
+++ watchlist/src/components/film/Hero.tsx
@@ -17,6 +17,8 @@ import { useNavigate } from "react-router-dom";
 import UnknownValuePlaceHolder from "../UnknownValuePlaceHolder";
 import getRunTime from "../../utils/getRuntime";
 import FilmType from "../FilmType";
+import { useGSAP } from "@gsap/react";
+import { gsap, Power0 } from "gsap";
 
 export default function Hero({
   film,
@@ -34,6 +36,7 @@ export default function Hero({
     getFilmInWatchlist,
     loading,
     setLoading,
+    canAnimate
   } = useContext(AppContext) as TAppContext;
   const {
     title,
@@ -156,6 +159,15 @@ export default function Hero({
     }
   }
 
+  useGSAP(() => {
+    if (!canAnimate || loading) return
+    const tl = gsap.timeline()
+    tl.fromTo('.text-to-reveal', { clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)' }, { clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)', duration: 1 })
+    tl.from('#film-cta-btn-cntr', { opacity: 0, ease: "sine.in", duration: .8 }, "<")
+    tl.fromTo('#scroll-indicator-thumb', { opacity: 0 }, { opacity: 1, ease: "sine.in" })
+    tl.to('#scroll-indicator-thumb', { repeat: -1, yoyo: true, y: 32, opacity: 1, duration: 1.5, ease: "sine.in" })
+  }, [canAnimate])
+
   return (
     <div className="min-h-[100%] flex flex-col gap-12 md:gap-0 justify-end md:flex-row md:justify-between md:items-center mt-5 p-[4%] pb-[10%] md:p-[2%]  font-inter font-normal md:font-light md:tracking-wide">
       <section className="text-to-reveal flex flex-col gap-2 md:gap-4 md:w-[50%]">
@@ -204,59 +216,24 @@ export default function Hero({
       </section>
 
       <section className="md:w-[45%]">
-        <div className="relative z-[5] flex justify-end md:justify-center items-center gap-2 md:gap-4">
+        <div id="film-cta-btn-cntr" className="relative z-[5] flex justify-end md:justify-center items-center gap-2 md:gap-4">
           <button
             className="group flex justify-center items-center px-5 py-4 md:p-5 bg-red-900/90 hover:bg-zinc-950/90 shadow-addButton hover:shadow-addButtonHover rounded-full cursor-pointer transition-transform active:translate-y-1 active:shadow-xl active:shadow-red-900/90"
             onClick={handleWatchlistBtnClick}
           >
             {loading ? (
-              <svg
-                className="w-8 md:w-[60px] stroke-zinc-900 group-hover:stroke-red-900/90"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 100 100"
-                preserveAspectRatio="xMidYMid"
-                style={{
-                  shapeRendering: "auto",
-                  display: "block",
-                  background: "transparent",
-                }}
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-              >
-                <g>
-                  <circle
-                    strokeDasharray="164.93361431346415 56.97787143782138"
-                    r="35"
-                    strokeWidth="10"
-                    fill="none"
-                    cy="50"
-                    cx="50"
-                  >
-                    <animateTransform
-                      keyTimes="0;1"
-                      values="0 50 50;360 50 50"
-                      dur="1s"
-                      repeatCount="indefinite"
-                      type="rotate"
-                      attributeName="transform"
-                    ></animateTransform>
-                  </circle>
+              <svg className="w-8 md:w-[60px] stroke-zinc-900 group-hover:stroke-red-900/90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{ shapeRendering: "auto", display: "block", background: "transparent", }} xmlnsXlink="http://www.w3.org/1999/xlink">
+                <g><circle strokeDasharray="164.93361431346415 56.97787143782138" r="35" strokeWidth="10" fill="none" cy="50" cx="50">
+                  <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+                </circle>
                   <g></g>
                 </g>
               </svg>
             ) : isFilmInWatchlist ? (
-              <svg
-                className="w-9 md:w-[60px] fill-zinc-900 group-hover:fill-red-900/90"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 -960 960 960"
-              >
-                <path d="M109.26-309.81v-88.52h321.41v88.52H109.26Zm0-178.22v-88.14h482.3v88.14h-482.3Zm0-177.84v-88.15h482.3v88.15h-482.3Zm550.38 494.03L506.56-324.91l61.74-62.41 91.34 90 180-179.81 62.97 62.88-242.97 242.41Z" />
+              <svg className="w-9 md:w-[60px] fill-zinc-900 group-hover:fill-red-900/90" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"> <path d="M109.26-309.81v-88.52h321.41v88.52H109.26Zm0-178.22v-88.14h482.3v88.14h-482.3Zm0-177.84v-88.15h482.3v88.15h-482.3Zm550.38 494.03L506.56-324.91l61.74-62.41 91.34 90 180-179.81 62.97 62.88-242.97 242.41Z" />
               </svg>
             ) : (
-              <svg
-                className="w-9 md:w-[60px] fill-zinc-900 group-hover:fill-red-900/90"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 -960 960 960"
-              >
+              <svg className="w-9 md:w-[60px] fill-zinc-900 group-hover:fill-red-900/90" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                 <path d="M109.26-309.14v-88.53h322.16v88.53H109.26Zm0-178.22v-88.15h481.48v88.15H109.26Zm0-177.84v-88.15h481.48v88.15H109.26Zm541.18 515.19v-159.13h-159.7v-88.53h159.7v-159.69h88.52v159.69h159.13v88.53H738.96v159.13h-88.52Z" />
               </svg>
             )}
@@ -270,7 +247,7 @@ export default function Hero({
         <div className="absolute left-6 bottom-2 md:left-auto md:right-2 flex gap-2">
           <div className="grid grid-cols-1 grid-rows-1">
             <span className="col-start-1 col-end-2 row-start-1 row-end-1 justify-self-center z-10 block w-[2px] h-14 bg-zinc-900 opacity-60"></span>
-            <span className="col-start-1 col-end-2 row-start-1 row-end-1 justify-self-center z-20 block w-[3px] h-6 bg-red-800 rounded-sm"></span>
+            <span id="scroll-indicator-thumb" className="col-start-1 col-end-2 row-start-1 row-end-1 justify-self-center z-20 block w-[3px] h-6 bg-red-800 rounded-sm"></span>
           </div>
           <p className="self-end w-6 md:w-14 leading-none font-robotoCondensed font-normal md:font-light text-sm md:tracking-wide">
             Scroll down
